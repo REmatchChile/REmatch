4986a0d713aaf51cd60fdaf7efa9c057c7d728d9
VicenteVicenteVicenteVicenteVicente <vecalisto@uc.cl>
2020-05-12 15:39:59 -0400

    feat: catch OOM, export overlay field

--- REmatchWeb/work.js
+++ REmatchWeb/work.js
@@ -6,28 +6,37 @@ var allSpans = [];
 
 Module['onRuntimeInitialized'] = () => {
     onmessage = (m) => {
-        var t0 = Date.now();
-        var instance = new Module.WasmInterface(m.data.text, m.data.query);
-        instance.init();
-        var tempSchema = instance.getOutputSchema();
-        for (var i = 0; i < tempSchema.size(); i++) {
-            schema.push(tempSchema.get(i));
-        }
-        while (instance.hasNext()) {
-            spanList = [];
-            curr = instance.next();
-            for (i = 0; i < schema.length; i++) {
-                spanList.push(curr.get(i));
+        try {
+            var t0 = Date.now();
+            var instance = new Module.WasmInterface(m.data.text, m.data.query);
+            instance.init();
+            var tempSchema = instance.getOutputSchema();
+            for (var i = 0; i < tempSchema.size(); i++) {
+                schema.push(tempSchema.get(i));
             }
-            allSpans = allSpans.concat([spanList]);
-        }
-        instance.delete();
-        postMessage(
-            {   
-                schema: schema,
-                spans: allSpans, 
-                log: `${allSpans.length} results in ${(Date.now() - t0) / 1000}s.`,
+            while (instance.hasNext()) {
+                spanList = [];
+                curr = instance.next();
+                for (i = 0; i < schema.length; i++) {
+                    spanList.push(curr.get(i));
+                }
+                allSpans = allSpans.concat([spanList]);
             }
-        )
+            instance.delete(); // Necessary?
+            postMessage(
+                {   
+                    schema: schema,
+                    spans: allSpans, 
+                    log: `${allSpans.length} results in ${(Date.now() - t0) / 1000}s.`,
+                }
+            )
+        } catch(err) {
+            instance.delete(); // Necessary?
+            postMessage(
+                {
+                    error: 'Out of memory! The worker will be restarted.',
+                }
+            ) 
+        }
     }
 }
