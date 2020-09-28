12156001b701f92bf1e96ccfc6f46793f9c242dc
Nicolás Van Sint Jan <nicovsj@uc.cl>
2020-09-28 13:49:49 -0300

    added line by line testing

--- REmatchEngine/src/main/test-lines.cpp
+++ REmatchEngine/src/main/test-lines.cpp
@@ -1,14 +1,14 @@
 #include <sstream>
 
+#include <sys/resource.h>
+
 #include "regex/regex.hpp"
 #include "document.hpp"
 #include "eval.hpp"
 #include "util.hpp"
 
 int main(int argc, char *argv[]) {
-  ChunkDocument doc;
-  rematch::RegEx rgx(".*!x{\\w+}.*");
-  rematch::Evaluator eval(rgx, doc);
+  rematch::RegEx rgx(".* !x{\\w+} .*");
 
   std::unique_ptr<rematch::Match> match;
 
@@ -18,9 +18,21 @@ int main(int argc, char *argv[]) {
   std::istringstream iss(text);
   std::string line;
   while(std::getline(iss, line, '\n')) {
+    std::cout << "In line: \"" << line << "\"\n";
     rgx.feed(line);
-    while(match = eval.next()) {
+    while(match = rgx.internalFindIter()) {
       std::cout << *match << std::endl;
     }
   }
+
+  // GET MEMORY USAGE
+	struct rusage usage;
+	int ret;
+	std::string memoryUsed;
+	ret = getrusage(RUSAGE_SELF, &usage);
+	if(ret == 0) {
+		memoryUsed = formatMem(usage.ru_maxrss*1024);
+	}
+
+  std::cout << "Mem used: " << memoryUsed << '\n';
 }
