92a1b9c73e7fd789e986e0ae140a9383fa8fdae8
Nicolás Van Sint Jan <nicovsj@uc.cl>
2021-01-12 08:10:50 -0300

    Reorg structure

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# REmatch
+# Implementation of constant delay algorithm for regular document spanners
+
+This implementation is based on the paper `Constant delay algorithms for regular document spanners` by Fernando Florenzano, Cristian Riveros, Martín Ugarte, Stijn Vansummeren and Domagoj Vrgoč.
+
+### Directory structure
+
+The C++ implementation is under `/src` folder.
+
+The `/exp` folder contains different experiments to compare our library with others.
+
+The `/tests` folder contains all the automatic tests for our code.
+
+### Build instructions
+
+```
+cmake -H. -Bbuild/Release
+cmake --build build/Release
+```
+
+If you want to use a debugger such as `gdb`, then you should add `-DCMAKE_BUILD_TYPE=Debug` in the first CMake command.
+
+### Command line use
+
+After building, the binary file will be located in the `build/Release/bin` folder. To try it simply run:
+
+```
+build/Release/bin/rematch --help
+```
+
+#### Examples:
+
+Get all spans corresponding to a single letter `a`:
+```
+build/Release/bin/rematch -d document.txt -e '.*!x{a}.*'
+```
+Get all spans corresponding to a pattern in a file:
+```
+build/Release/bin/rematch -d document.txt -r regex.txt
+```
+Get benchmark stats (execution time, number of outputs, memory usage, etc.):
+```
+build/Release/bin/rematch -d document.txt -r regex.txt -o benchmark
+```
+
+
+### Testing
+
+We are using Boost.Test for unit testing.
+
+To add more tests, add a new folder inside `tests/[test_name_folder]/` that starts with the word _test_ as a
+prefix. Follow the same structure (same file names) of the other folders.
---
+++ src/automata/macrodfa/macrotransition.hpp
@@ -0,0 +1,79 @@
+#ifndef AUTOMATA_MACRODFA_MACROTRANSITION
+#define AUTOMATA_MACRODFA_MACROTRANSITION
+
+#include <bitset>
+#include <vector>
+#include <memory>
+
+#include "automata/dfa/detstate.hpp"
+#include "structs/prevector.hpp"
+
+class MacroState;
+
+struct MTCapture {
+  MTCapture() = default;
+  MTCapture(DetState& from, std::bitset<32> S, DetState& to)
+    : from(&from), to(&to), S(S) {}
+
+  DetState* from;
+  DetState* to;
+  std::bitset<32> S;
+};
+
+struct MTDirect {
+  MTDirect() = default;
+  MTDirect(DetState& from, DetState& to)
+    : from(&from), to(&to) {}
+
+  DetState* from;
+  DetState* to;
+};
+
+enum MacroType {
+  kOneDirect,
+  kTwoDirects,
+  kOther
+};
+
+#ifdef MACRO_TRANSITIONS_RAW_ARRAYS
+using DirectsArray = MTDirect*;
+using CapturesArray = MTCapture*;
+#else
+using DirectsArray = rematch::prevector<MTDirect>;
+using CapturesArray = rematch::prevector<MTCapture>;
+#endif
+
+class MacroTransition {
+ public:
+  MacroTransition();
+
+  MacroTransition(size_t ndirects, size_t ncaptures);
+
+  void add_direct(DetState& from, DetState& to);
+  void add_capture(DetState& from, std::bitset<32> S, DetState& to);
+
+  DirectsArray directs();
+  CapturesArray captures();
+
+  void set_next_state(MacroState* ms);
+
+  MacroState* next_state();
+
+  size_t ndirects_;
+  size_t ncaptures_;
+
+ private:
+  #ifdef MACRO_TRANSITIONS_RAW_ARRAYS
+  MTDirect *directs_;
+  MTCapture *captures_;
+  #else
+  rematch::prevector<MTDirect> directs_;
+  rematch::prevector<MTCapture> captures_;
+  #endif
+
+  MacroState* next_;
+
+  // MacroType type_;
+}; // end class MacroTransition
+
+#endif // AUTOMATA_MACRODFA_MACROTRANSITION
