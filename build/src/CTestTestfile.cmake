d6825dacaa0e7aec0b561892c30947f174231c07
Nicolás Van Sint Jan <nicovsj@uc.cl>
2021-01-12 08:21:30 -0300

    rm cached files

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nicovsj/repos/research/REmatch-org/REmatch/src
-# Build directory: /home/nicovsj/repos/research/REmatch-org/REmatch/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
