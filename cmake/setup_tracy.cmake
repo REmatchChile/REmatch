0a05f723c4eab14f7de80ea72cd75a26f0305cb7
gustavo-toro <gustavo.toro@uc.cl>
2024-01-18 17:15:07 -0300

    added tracy setup in cmake

---
+++ cmake/setup_tracy.cmake
@@ -0,0 +1,24 @@
+include(FetchContent)
+
+message("Fetching Tracy")
+
+Set(FETCHCONTENT_QUIET FALSE)
+
+FetchContent_Declare (
+  tracy
+  GIT_REPOSITORY https://github.com/wolfpld/tracy.git
+  GIT_TAG master
+  GIT_SHALLOW TRUE
+  GIT_PROGRESS TRUE
+)
+
+if (ENABLE_PROFILING)
+  set(TRACY_ENABLE ON CACHE BOOL "Enable Tracy" FORCE)
+  set(TRACY_NO_EXIT ON CACHE BOOL "No Exit Tracy" FORCE)
+  set(TRACY_DEBUGINFOD ON CACHE BOOL "Use DEBUGINFOD Tracy" FORCE)
+else()
+  set(TRACY_ENABLE OFF CACHE BOOL "Disable Tracy" FORCE)
+endif()
+
+FetchContent_MakeAvailable(tracy)
+target_compile_options(TracyClient PRIVATE -w)
