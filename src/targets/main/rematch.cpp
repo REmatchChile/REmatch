3e2a94448cdbe66c228ada528b00b269f0bc7667
VicenteVicente <vicente.calisto98@gmail.com>
2024-02-08 10:29:51 -0300

    removed extra executabes

--- src/targets/main/rematch.cpp
+++ src/targets/main/rematch.cpp
@@ -1,20 +1,23 @@
 #include "library_interface/rematch.hpp"
-#include <string_view>
 #include <iostream>
-
+#include <string_view>
 
 int main() {
-  std::string document = "This is a document";
-  std::string pattern = "!x{doc|document}";
-  // REMatch::Regex word_regex = REMatch::compile(pattern, REMatch::Flags());
-  // std::unique_ptr<REMatch::MatchIterator> iterator = word_regex.finditer(document);
-  // auto match = iterator->next();
-  // while (match != nullptr) {
-  //   std::cout << "Span: <" << match->start("x") << ',' << match->end("x") << '>'
-  //             << std::endl;
-  //   std::cout << match->group("x") << std::endl;
-  //   match = iterator->next();
-  // }
+  std::string pattern = "!a{a+}!b{b+}";
+  std::string document = "aabb";
+  REMatch::Flags flags;
+
+  REMatch::Regex word_regex = REMatch::compile(pattern, flags);
+  std::unique_ptr<REMatch::MatchIterator> iterator =
+      word_regex.finditer(document);
+  auto match = iterator->next();
+  auto variables = iterator->variables();
+  while (match != nullptr) {
+    for (auto& variable : variables) {
+      std::cout << variable << ": \"" << match->group(variable) << "\"" << std::endl;
+    }
+    match = iterator->next();
+  }
 
   return 0;
 }
