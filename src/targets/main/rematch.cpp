e68773eb56763252ead054e7b74c110b90c6f079
VicenteVicente <vicente.calisto98@gmail.com>
2024-02-09 12:38:12 -0300

    updated default target cpp

--- src/targets/main/rematch.cpp
+++ src/targets/main/rematch.cpp
@@ -1,23 +1,27 @@
 #include "library_interface/rematch.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <string_view>
 
-int main() {
-  std::string pattern = R"((,|^)!user{\w+}@!domain{\w+\.\w+}(,|$))";
-  std::string document = "vecalisto@uc.cl,test@gmail.com,jejjee@jaja.cl";
-  REMatch::Flags flags;
+int main(int argc, char* argv[]) {
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " <pattern> <document>" << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::string pattern = argv[1];
+  std::string document = argv[2];
 
-  REMatch::Regex word_regex = REMatch::compile(pattern, flags);
-  std::unique_ptr<REMatch::MatchIterator> iterator =
-      word_regex.finditer(document);
+  REMatch::Flags flags;
+  REMatch::Regex regex = REMatch::compile(pattern, flags);
+  std::unique_ptr<REMatch::MatchIterator> iterator = regex.finditer(document);
+
   auto match = iterator->next();
   auto variables = iterator->variables();
   while (match != nullptr) {
-    for (auto& variable : variables) {
-      std::cout << variable << ": \"" << match->group(variable) << "\"" << std::endl;
-    }
+    for (auto& variable : variables)
+      std::cout << variable << ": \"" << match->group(variable) << "\" |" << match->start(variable) << "," << match->end(variable) <<  ">" << std::endl;
     match = iterator->next();
   }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
