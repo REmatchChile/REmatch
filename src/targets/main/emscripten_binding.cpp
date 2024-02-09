8c4dfe6707c84d873656e597a9a0b981f75712fd
VicenteVicente <vicente.calisto98@gmail.com>
2024-02-09 12:33:12 -0300

    fix: patched emscripten

--- src/targets/main/emscripten_binding.cpp
+++ src/targets/main/emscripten_binding.cpp
@@ -10,19 +10,33 @@
 using namespace emscripten;
 using namespace REMatch;
 
+/**
+ * As Emscripten doesn't support std::string_view as argument, we have to bind
+ * the to std::string by copy instead.
+ */
+class RegexEmscriptenWrapper : public Regex {
+ public:
+  explicit RegexEmscriptenWrapper(std::string pattern)
+      : Regex(std::string_view(pattern)) {}
+
+  std::unique_ptr<MatchIterator> finditer_wrapper(std::string str) {
+    return finditer(std::string_view(str));
+  }
+};
+
 EMSCRIPTEN_BINDINGS(REmatch) {
-  class_<Flags>("Flags")
-    .constructor<>();
+  class_<RegexEmscriptenWrapper>("Regex")
+      .constructor<std::string>()
+      .function("finditer", &RegexEmscriptenWrapper::finditer_wrapper);
+
   class_<MatchIterator>("MatchIterator")
-    .function("next", &MatchIterator::next)
-    .function("variables", &MatchIterator::variables);
+      .function("next", &MatchIterator::next)
+      .function("variables", &MatchIterator::variables);
+
   class_<Match>("Match")
-    .function("variables", &Match::variables)
-    .function("start", select_overload<int(std::string)>(&Match::start))
-    .function("end", select_overload<int(std::string)>(&Match::end));
-  class_<Regex>("Regex")
-    .constructor<const std::string&, Flags>()
-    .function("finditer", &Regex::finditer);
+      .function("variables", &Match::variables)
+      .function("start", select_overload<int(std::string)>(&Match::start))
+      .function("end", select_overload<int(std::string)>(&Match::end));
 
   register_vector<std::string>("VectorString");
 };
