d627925f6e692fb779f18a4a88b771f9368a69aa
VicenteVicente <vicente.calisto98@gmail.com>
2024-12-26 13:05:27 -0300

    fixed tracy in project

--- cmake/setup_tracy.cmake
+++ cmake/setup_tracy.cmake
@@ -1,14 +1,23 @@
-include(FetchContent)
+if (ENABLE_PROFILING)
+  include(FetchContent)
 
-message("Fetching Tracy")
+  message("Fetching Tracy")
 
-Set(FETCHCONTENT_QUIET FALSE)
+  Set(FETCHCONTENT_QUIET FALSE)
 
-FetchContent_Declare (
-  tracy
-  GIT_REPOSITORY https://github.com/wolfpld/tracy.git
-  GIT_TAG master
-  GIT_SHALLOW TRUE
-)
+  FetchContent_Declare (
+    tracy
+    GIT_REPOSITORY https://github.com/wolfpld/tracy.git
+    GIT_TAG master
+    GIT_SHALLOW TRUE
+  )
 
-FetchContent_MakeAvailable(tracy)
+  set(TRACY_ENABLE ON CACHE BOOL "Enable Tracy" FORCE)
+  set(TRACY_NO_EXIT ON CACHE BOOL "No Exit Tracy" FORCE)
+  set(TRACY_DEBUGINFOD ON CACHE BOOL "Use DEBUGINFOD Tracy" FORCE)
+  add_definitions(-DTRACY_ENABLE)
+
+  FetchContent_MakeAvailable(tracy)
+else()
+  set(TRACY_ENABLE OFF CACHE BOOL "Disable Tracy" FORCE)
+endif()
